aa0ab319741fbe7aa7c5fbed1e90bcd7e05603a3
RupomAhsan <rupom@live.com>
2017-11-28 12:31:12 +0800

    Basic Unit Test Methods

---
+++ MyUnitClassesTest/FileProcessTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyUnitClasses;
+
+namespace MyUnitClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\WrongFileName.wrong");
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            FileProcess fp = new FileProcess();
+            fp.FileExists("");
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException_UsingTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentNullException)
+            {
+                //The test was a success
+                return;
+            }
+
+            Assert.Fail("Call to file FileExists did NOT thrw an ArgumentNUllException.");
+
+        }
+    }
+}
