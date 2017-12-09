0da4b64ae3cf6bc86a396550422a108ee2d590f7
RupomAhsan <rupom@live.com>
2017-12-10 02:28:33 +0800

    StringAssert Test

---
+++ MyUnitClassesTest/StringAssertClassTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace MyUnitClassesTest
+{
+    [TestClass]
+    public class StringAssertClassTest
+    {
+        #region Contains / StartsWith / RegEx Tests 
+        [TestMethod]
+        [Owner("Rupom")]
+        public void ContainsTest()
+        {
+            string str1 = "Rupom Ahsan";
+            string str2 = "Rupom";
+
+            StringAssert.Contains(str1, str2);
+        }
+
+        [TestMethod]
+        [Owner("Rupom")]
+        public void StartsWithTest()
+        {
+            string str1 = "Rupom Ahsan";
+            string str2 = "Rupom";
+
+            StringAssert.StartsWith(str1, str2);
+        }
+
+        [TestMethod]
+        [Owner("Rupom")]
+        public void IsAllLowerCaseTest()
+        {
+            Regex r = new Regex(@"^([^A-Z])+$");
+
+            StringAssert.Matches("all lower case", r);
+        }
+
+        [TestMethod]
+        [Owner("Rupom")]
+        public void IsNotAllLowerCaseTest()
+        {
+            Regex r = new Regex(@"^([^A-Z])+$");
+
+            StringAssert.DoesNotMatch("All Lower Case", r);
+        }
+        #endregion
+    }
+}
