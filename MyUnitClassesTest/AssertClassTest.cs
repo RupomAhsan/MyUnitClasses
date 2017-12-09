9a422141a5597e092e0b8bc6b20e2063e46d6065
RupomAhsan <rupom@live.com>
2017-12-10 01:46:35 +0800

    Test String values

---
+++ MyUnitClassesTest/AssertClassTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyUnitClassesTest
+{
+    [TestClass]
+    public class AssertClassTest
+    {
+        #region AreEqual / AreNotEqual Tests 
+        [TestMethod]
+        [Owner("Rupom")]
+        public void AreEqualTest()
+        {
+            string str1 = "Rupom";
+            string str2 = "Rupom";
+
+            Assert.AreEqual(str1, str2);
+        }
+
+        [TestMethod]
+        [Owner("Rupom")]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void AreEqualCaseSensativeTest()
+        {
+            string str1 = "Rupom";
+            string str2 = "rupom";
+
+            Assert.AreEqual(str1, str2,false);
+        }
+
+        [TestMethod]
+        [Owner("Ahsan")]
+        public void AreNotEqualTest()
+        {
+            string str1 = "Rupom";
+            string str2 = "Ahsan";
+
+            Assert.AreNotEqual(str1, str2);
+        }
+        #endregion
+    }
+}
