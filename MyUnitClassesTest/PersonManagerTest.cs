ade065a261af8d61e291bd66dbfb94c165152b20
RupomAhsan <rupom@live.com>
2017-12-10 17:56:17 +0800

    IsInstanceOfType Test

---
+++ MyUnitClassesTest/PersonManagerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyUnitClasses.PersonClasses;
+
+namespace MyUnitClassesTest
+{
+    [TestClass]
+    public class PersonManagerTest
+    {
+        [TestMethod]
+        public void CreatePerson_OfTypeEmployeeTest()
+        {
+            PersonManager mgr = new PersonManager();
+            Person per;
+
+            per = mgr.CreatePerson("Rupom", "Ahsan", false);
+
+            Assert.IsInstanceOfType(per, typeof(Employee));
+        }
+    }
+}
